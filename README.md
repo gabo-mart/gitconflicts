b75801030267e2b7d4de3b22f5fafd3bb717fa5a
Gabo Martinez <gabmart02@gmail.com>
2016-04-25 17:12:35 -0400

    the silent

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 BRAINFARTS V1.1
 
 The silently flatulent programming language.
+
+silent but deadly
--- sequel.sql
+++ sequel.sql
@@ -4,3 +4,16 @@ CREATE TABLE person (
     last_name TEXT,
     age INTEGER
 );
+
+CREATE TABLE pet (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    breed TEXT,
+    age INTEGER,
+    dead INTEGER
+);
+
+CREATE TABLE person_pet (
+    person_id INTEGER,
+    pet_id INTEGER
+);
